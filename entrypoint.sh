0839bfd219e40f259e678b51c628fb6ddcb9ca06
Liu Mingmin <diveintotomorrow@gmail.com>
2020-07-28 11:28:33 +0800

    set output of action

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,10 +2,12 @@
 
 resp_code=$(curl -s -o resp.log -w "%{http_code}" -XPOST ${INPUT_KALM_API_ADDRESS}/__kalm_webhook/deploy?deploy-key=${INPUT_KALM_DEPLOY_KEY}\&app=${INPUT_KALM_APP}\&component=${INPUT_KALM_COMPONENT}\&image-tag=${INPUT_KALM_COMPONENT_IMG_TAG})
 
+resp=$(cat resp.log)
+echo "::set-output name=resp::$resp"
+
 if [ $resp_code == 200 ] 
 then
     exit 0
 else
-    cat resp.log
     exit 1
 fi
