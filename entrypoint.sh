3de784b19e0c518f73e5cc3c2bf5de7271b699a6
Liu Mingmin <diveintotomorrow@gmail.com>
2020-07-28 11:50:35 +0800

    echo resp

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,6 +3,8 @@
 resp_code=$(curl -s -o resp.log -w "%{http_code}" -XPOST ${INPUT_KALM_API_ADDRESS}/__kalm_webhook/deploy?deploy-key=${INPUT_KALM_DEPLOY_KEY}\&app=${INPUT_KALM_APP}\&component=${INPUT_KALM_COMPONENT}\&image-tag=${INPUT_KALM_COMPONENT_IMG_TAG})
 
 resp=$(cat resp.log)
+
+echo $resp
 echo "::set-output name=resp::$resp"
 
 if [ $resp_code == 200 ] 
