6d55409cd0c7a2c6c83611abd2b6c9595fcf9833
Liu Mingmin <diveintotomorrow@gmail.com>
2020-07-29 09:30:40 +0800

    update reqeust to webhook

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,17 @@
 #!/bin/sh -l
 
-resp_code=$(curl -s -o resp.log -w "%{http_code}" -XPOST ${INPUT_KALM_API_ADDRESS}/__kalm_webhook/deploy?deploy-key=${INPUT_KALM_DEPLOY_KEY}\&app=${INPUT_KALM_APP}\&component=${INPUT_KALM_COMPONENT}\&image-tag=${INPUT_KALM_COMPONENT_IMG_TAG})
+cat <<EOF > data.json
+{
+    "deployKey":     "${INPUT_KALM_DEPLOY_KEY}",
+    "application":   "${INPUT_KALM_APP}",
+    "componentName": "${INPUT_KALM_COMPONENT}",
+    "imageTag":      "${INPUT_KALM_COMPONENT_IMG_TAG}"
+}
+EOF
+
+resp_code=$(curl -s -o resp.log -w "%{http_code}" -XPOST -H "Content-Type: application/json" -d "@data.json" ${INPUT_KALM_API_ADDRESS}/webhook/components)
+
+rm data.json
 
 resp=$(cat resp.log)
 
